dd36f9032aa07a12dc3e56f1c35a76e4d28d7c02
Thomas Boop <tgboop@ncsu.edu>
2017-08-03 15:06:01 -0400

    added fixes

--- javascript-sample.js
+++ javascript-sample.js
@@ -5,10 +5,10 @@
     const smokeDirections = ['forward', 'backward', 'left', 'right', 'drop'];
     const smokeDirection = smokeDirections[Math.floor(Math.random() * 5)];
     //check south
-    sClear = (state.arena[2][3].contents.type === "open" || state.arena[2][3].contents.type === "food");
-    nClear = (state.arena[4][3].contents.type === "open" || state.arena[4][3].contents.type === "food");
-    eClear = (state.arena[3][4].contents.type === "open" || state.arena[3][4].contents.type === "food")
-    wClear = (state.arena[3][2].contents.type === "open" || state.arena[3][2].contents.type === "food")
+    sClear = (state.arena[2][3].contents.type == "open" || state.arena[2][3].contents.type == "food");
+    nClear = (state.arena[4][3].contents.type == "open" || state.arena[4][3].contents.type == "food");
+    eClear = (state.arena[3][4].contents.type == "open" || state.arena[3][4].contents.type == "food");
+    wClear = (state.arena[3][2].contents.type == "open" || state.arena[3][2].contents.type == "food");
     if ("s" == state.arena[3][3].contents.orientation) {
         if (sClear){
             return {
